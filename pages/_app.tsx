d8cb155bce271dbfb3b51f4b3820d954e55dedf2
MinWooByun <yja05092@gmail.com>
2022-12-13 19:38:16 +0900

    ApolloProvider
    Provider는 기본적으로 애플리케이션 안의 모두가 client에 접근할 수 있게 해준다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import client from "./client";
+import { ApolloProvider } from "@apollo/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   client;
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
+import { gql, useApolloClient } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [movies, setMovies] = useState<any>([]);
+  const client = useApolloClient();
+
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              id
+              title
+            }
+          }
+        `,
+      })
+      .then((results) => setMovies(results.data.allMovies));
+  }, [client]);
+
   return (
     <div>
-      <h1>This is a list of movies</h1>
+      <h1>
+        {movies.map((movie: any) => {
+          return <ul key={movie.id}>{movie.title}</ul>;
+        })}
+      </h1>
     </div>
   );
 };
